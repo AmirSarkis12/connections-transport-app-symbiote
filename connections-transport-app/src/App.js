c2da3025f2df2b5a544942cc3b9c4e4d00f82c51
Amir Sarkis <sarkis.amir@gmail.com>
2019-06-06 19:48:14 +1000

    Added example.data.js
    - Able to import data from example.data.js

--- connections-transport-app/src/App.js
+++ connections-transport-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import DataSet from './example-data'
 
 function App() {
   return (
     <div className="App">
+     {DataSet.routes.map(route => route.title)}
     </div>
   );
 }
